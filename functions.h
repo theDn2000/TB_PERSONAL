024c5b3b52f498c3c261bd99e9ed935f543f7967
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-13 09:27:40 +0200

    functions.h

--- functions.h
+++ functions.h
@@ -1,14 +1,8 @@
 #pragma once
 
-#include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
-
 // Suma
 int suma(int a, int b);
 int sumaP(int a, int b);
 
 // Función parsea string
-vector<string> separate_string(string & s)
+std::vector<std::string> separate_string(string & s)
