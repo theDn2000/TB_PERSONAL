7644b920a7c3e70abda3de192fa7611e01099b77
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-13 09:28:48 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -5,4 +5,4 @@ int suma(int a, int b);
 int sumaP(int a, int b);
 
 // Función parsea string
-std::vector<std::string> separate_string(string & s)
+std::vector<std::string> separate_string(std::string & s)
