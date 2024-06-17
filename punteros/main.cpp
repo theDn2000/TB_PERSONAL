633d1e31dbe680a9fda0e35f5a62f4b1dcb6449e
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-17 08:50:23 +0200

    punteros

---
+++ punteros/main.cpp
@@ -0,0 +1,51 @@
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+void modifica(int x)
+{
+    x = 11;
+}
+void modificaP(shared_ptr<int> pX)
+{
+    *pX = 11;
+}
+
+int suma(int a, int b)
+{
+    return a + b;
+}
+
+int sumaP(shared_ptr<int> pA, shared_ptr<int> pB)
+{
+    return *pA + *pB;
+}
+
+int main()
+{
+    int a = 5;
+    shared_ptr<int> pA = make_shared<int>(5);
+    shared_ptr<int> pB = pA;
+    int b = a;
+
+    *pB = 7;
+    b = 7;
+
+    cout << "a: " << a << endl;
+    cout << "pA: " << *pA << endl;
+
+    modifica(a);
+    modificaP(pA);
+
+    cout << "a modificado: " << a << endl;
+    cout << "pA modificado: " << *pA << endl;
+
+    int ab = suma(a, b);
+    int pApB = sumaP(pA, pB);
+
+    cout << "a + b: " << ab << endl;
+    cout << "pA + pB: " << pApB << endl;
+
+    return 0;
+}
