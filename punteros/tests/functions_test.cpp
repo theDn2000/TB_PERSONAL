6e67c2c0becde0d710abf8559264bb84f1a4ccaa
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-18 10:59:10 +0200

    Update functions_test.cpp

--- punteros/tests/functions_test.cpp
+++ punteros/tests/functions_test.cpp
@@ -127,7 +127,7 @@ TEST(DifferenceTest, difference_difference)
     vector<shared_ptr<Person>> personas = {p1, p2, p3};
     vector<shared_ptr<Person>> personas2 = {p2, p3};
 
-    vector<shared_ptr<Person>> diferencia = difference(personas, personas2);
+    vector<shared_ptr<Person>> diferencia = difference(personas2, personas);
     EXPECT_EQ(diferencia.size(), 1);
 }
 
