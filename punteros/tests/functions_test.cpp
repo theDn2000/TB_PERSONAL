d64c8f1f1df742606a117dff6d2c7e374f8077c4
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-18 08:46:53 +0200

    Update functions_test.cpp

--- punteros/tests/functions_test.cpp
+++ punteros/tests/functions_test.cpp
@@ -11,8 +11,8 @@ using namespace std;
 TEST(IncludeTest, include_incluye)
 {
     shared_ptr<Person> p1 = make_shared<Person>("Juan", 30, "12345678A", "juan@mail.com", false);
-    shared_ptr<Person> p2 = make_shared<Person>{"Pedro", 30, "12345678A", "pedro@mail.com", false};
-    shared_ptr<Person> p3 = make_shared<Person>{"Maria", 40, "12345678A", "maria@mail,com", false};
+    shared_ptr<Person> p2 = make_shared<Person>(Person{"Pedro", 30, "12345678A", "pedro@mail.com", false});
+    shared_ptr<Person> p3 = make_shared<Person>(Person{"Maria", 40, "12345678A", "maria@mail,com", false});
     vector<shared_ptr<Person>> personas = {p1, p2, p3};
     EXPECT_TRUE(include(personas, p1));
     EXPECT_TRUE(include(personas, p2));
@@ -22,10 +22,10 @@ TEST(IncludeTest, include_incluye)
 // Compruebo que si no incluye, devuelve false
 TEST(IncludeTest, include_no_incluye)
 {
-    shared_ptr<Person> p1 = make_shared<Person>{"Juan", 30, "12345678A", "juan@mail.com", false};
-    shared_ptr<Person> p2 = make_shared<Person>{"Pedro", 30, "12345678A", "pedro@mail.com", false};
-    shared_ptr<Person> p3 = make_shared<Person>{"Maria", 40, "12345678A", "maria@mail,com", false};
-    shared_ptr<Person> p4 = make_shared<Person>{"Ana", 50, "12345678A", "ana@mail.com", false};
+    shared_ptr<Person> p1 = make_shared<Person>(Person{"Juan", 30, "12345678A", "juan@mail.com", false});
+    shared_ptr<Person> p2 = make_shared<Person>(Person{"Pedro", 30, "12345678A", "pedro@mail.com", false});
+    shared_ptr<Person> p3 = make_shared<Person>(Person{"Maria", 40, "12345678A", "maria@mail,com", false});
+    shared_ptr<Person> p4 = make_shared<Person>(Person{"Ana", 50, "12345678A", "ana@mail.com", false});
     vector<shared_ptr<Person>> personas = {p1, p2, p3};
     EXPECT_FALSE(include(personas, p4));
 }
