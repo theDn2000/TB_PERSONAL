b779f0152348d115e9b7f77c6bb2a4e6ddc92deb
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-18 11:52:52 +0200

    funciones lambda

---
+++ lambda/main.cpp
@@ -0,0 +1,33 @@
+#include <functional>
+#include <iostream>
+
+using namespace std;
+
+
+int operacion(int a, int b, function<int(int,int)> f)
+{
+    return f(a,b);
+}
+
+int main ()
+{
+    function<int(int,int)> suma = [](int a, int b)
+    {
+        return a + b;
+    };
+
+    function<int(int,int)> resta = [](int a, int b)
+    {
+        return a - b;
+    };
+    
+    function<int(int,int)> multiplicacion = [](int a, int b)
+    {
+        return a * b;
+    };
+
+    auto h = operacion(1, 2, suma); // Función lambda o funcion de callback
+    auto m = operacion(1, 2, multiplicacion);
+
+    return 0;
+}
