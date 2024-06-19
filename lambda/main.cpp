6fe312c0ce5a43e709a4837f6a2c7a010407661b
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-19 08:10:12 +0200

    Update main.cpp

--- lambda/main.cpp
+++ lambda/main.cpp
@@ -1,17 +1,30 @@
 #include <functional>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+// Alias
+typedef vector<int> vi;
+typedef function<void(int)> flambda;
 
 int operacion(int a, int b, function<int(int,int)> f)
 {
     return f(a,b);
 }
 
+// For Each --> Recorre un vector y hace algo con cada elemento
+void forEach(vi const &v, flambda const &f)
+{
+    for (auto elem : v)
+    {
+        f(elem);
+    }
+}
+
 int main ()
 {
-    function<int(int,int)> suma = [](int a, int b)
+    function<int(int,int)> suma = [](int a, int b) 
     {
         return a + b;
     };
@@ -26,11 +39,22 @@ int main ()
         return a * b;
     };
 
+
+    flambda print = [](int a)
+    {
+        cout << a << endl;
+    };
+
+    vi v = {1, 2, 3, 4, 5};
+
+    forEach(v, print);
+
+    /*
     auto h = operacion(1, 2, suma); // Función lambda o funcion de callback
     auto m = operacion(1, 2, multiplicacion);
 
     cout << "Suma: " << h << endl;
     cout << "Multiplicación: " << m << endl;
-
+    */
     return 0;
 }
