7f5f6fd3ab3cf202a8e659233662d7f60fa65cbe
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-20 08:43:02 +0200

    Update main.cpp

--- lambda/main.cpp
+++ lambda/main.cpp
@@ -227,11 +227,11 @@ int main()
     // Serie fibonacci
     // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
     // Fibonnacci with for each
-    vector<int> fibo{0, 1};
+    vector<int> fibo{0, 1, 2};
     vector<int> n{1,2,3,4,5,6,7,8,9,10};
 
 
-    forEachint(n, [&fibo](int elem, int i)
+    forEachint(n, [&](int elem, int i)
     {
         int size = fibo.size();
         fibo.push_back(fibo.at(size - 1) + fibo.at(size - 2));
