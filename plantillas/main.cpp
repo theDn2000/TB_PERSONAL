d8c04609c5db61be7b5c6c4e913724dece185ac5
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-20 12:13:47 +0200

    a

---
+++ plantillas/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+// Struct person
+struct Person
+{
+    string name;
+    int age;
+};
+
+// Templatizar 
+template <typename T>
+T suma (T a, T b)
+{
+    return a + b;
+}
+
+// Templatizar varios tipos de datos
+template <typename T, typename U>
+T pipo(T a, U b)
+{
+    return a + b;
+}
+
+Person suma (Person a, Person b)
+{
+    Person p;
+    p.name = a.name + b.name;
+    p.age = a.age + b.age;
+    return p;
+}
+
+// Templatizar for each
+template <typename T, typename U>
+void forEach(vector<T> const &v, function<U(T)> const &f)
+{
+    for (auto elem : v)
+    {
+        f(elem);
+    }
+};
+
+
+int main ()
+{
+    // Plantillas: Funciones a las que no les queremos especificar el tipo de dato
+    auto c = suma<int>(5, 6);
+    return 0;
+}
