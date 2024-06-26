cbcdaa31ad7bfca351062fbbd523419129fde437
Daniel Carneros Mateu <109517814+theDn2000@users.noreply.github.com>
2024-06-26 11:07:06 +0200

    Update main.cpp

--- listas/main.cpp
+++ listas/main.cpp
@@ -30,13 +30,13 @@ bool operator >(Person const &p1, Person const &p2)
 
 ostream &operator<< (ostream &os, Person const &p)
 {
-    os << p.name << p.telephone;
+    os << p.name << " " << p.telephone;
     return os;
 }
 
 istream &operator>> (istream &is, Person &p)
 {
-    is >> p.name >> p.telephone;
+    is >> p.name >> " " >> p.telephone;
     return is;
 }
 
