4a103b972e7b5e4db8b806212b3a71f7c4352b92
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-27 08:37:36 +0200

    Update main.cpp

--- repaso/main.cpp
+++ repaso/main.cpp
@@ -6,6 +6,7 @@ int main()
 {
     Student st1{"1234", "Alberto", {"Progra 1", "Progra 2", "EDA"}};
     Student st2{"1234", "Luisa", {"Progra 1", "Progra 2", "EDA"}};
+    cout << st1 << endl;
     PNode head = nullptr;
     push(head, st1);
 
