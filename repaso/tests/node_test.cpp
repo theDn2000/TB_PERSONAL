9e09c5a2a9608cc505ef5975a66c98c24516840b
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-27 09:09:54 +0200

    Update node_test.cpp

--- repaso/tests/node_test.cpp
+++ repaso/tests/node_test.cpp
@@ -10,12 +10,12 @@ using namespace std;
 // Compruebo que si incluye, devuelve true
 TEST(Push, push_head_nullptr)
 {
-    Student st1{"1234", "Alberto", {"Progra 1", "Progra 2", "EDA"}};
-    Student st2{"1234", "Luisa", {"Progra 1", "Progra 2", "EDA"}};
-    PNode head = nullptr;
-    head = push(head, st1);
-    EXPECT_TRUE(head !=nullptr);
-    EXPECT_EQ(head->data, st1); // Comparacion estudiantes
+  Student st1{"1234", "Alberto", {"Progra I", "Progra 2", "EDA"}};
+  // Student st2{"4321", "Luisa", {"Progra I", "Progra 2", "EDA"}};
+  PNode head = nullptr;
+  head = push(head, st1);
+  EXPECT_TRUE(head != nullptr);
+  EXPECT_EQ(head->data, st1);
 }
 
 // COmpruebo funcion filter
