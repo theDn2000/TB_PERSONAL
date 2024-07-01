098242b3f84943fd7e1c89f0a5d28f64f59e7568
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-07-01 08:48:38 +0200

    Update main.cpp

--- superejemplo/main.cpp
+++ superejemplo/main.cpp
@@ -21,17 +21,58 @@ int main()
   cout << "|3. Filtrar estudiantes                      |" << endl;
   cout << "|4. Buscar estudiante (primero)              |" << endl;
   cout << "|5. Buscar estudiante (ultimo)               |" << endl;
-  cout << "|6. Salir                                    |" << endl;
+  cout << "|6. Escribir en archivo                      |" << endl;
+  cout << "|7. Leer de archivo                          |" << endl;
+  cout << "|8. Salir                                    |" << endl;
   cout << "----------------------------------------------" << endl;
 
 
+  while (true)
+  {
+    int option;
+    cin >> option;
+    string name;
+    PNode node;
+    Student st;
 
-
-
-
-
-
-
-
+    switch (option)
+    {
+    case 1:
+      cout << "Ingrese los datos del estudiante: (name Alberto Valero)(subjects Progra I Progra 2 EDA)" << endl;
+      cin >> st;
+      push(head, st);
+      break;
+    case 2:
+      forEach(head, [](Student const &st)
+              { cout << st << endl; });
+      break;
+    case 3:
+      cout << "Ingrese el nombre o parte del nombre para filtrar: ";
+      cin >> name;
+      filter(head, [name](Student const &st)
+            { return st.name.find(name) != string::npos; });
+      break;
+    case 4:
+      cout << "Ingrese el nombre o parte del nombre para buscar: ";
+      cin >> name;
+      node = find(head, [name](Student const &st)
+                      { return st.name.find(name) != string::npos; });
+      cout << node->data << endl;
+      break;
+    case 5:
+      cout << "Ingrese el nombre o parte del nombre para buscar: ";
+      cin >> name;
+      node = findLast(head, [name](Student const &st)
+                          { return st.name.find(name) != string::npos; });
+      cout << node->data << endl;
+      break;
+    case 8:
+      return 0;
+      break;
+    default:
+      cout << "Opción no válida" << endl;
+      break;
+    }
+  }
   return 1;
 }
