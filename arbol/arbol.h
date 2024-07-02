fce2817cd9c293ecb8d739a9319ed7c1d0f16b9f
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-07-02 09:33:22 +0200

    arbol v2.3 find

--- arbol/arbol.h
+++ arbol/arbol.h
@@ -11,17 +11,17 @@ struct Node;
 
 using P_Node = shared_ptr<Node>;
 
+struct Data
+{
+    int value;
+    int id;
+};
+
 struct Node
 {
     Data data;
     shared_ptr<Node> left;
     shared_ptr<Node> right;
-};
-
-struct Data
-{
-    int value;
-    int id;
 };
 
 P_Node push(P_Node &head, int data);
