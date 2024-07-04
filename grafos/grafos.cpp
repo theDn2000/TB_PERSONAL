50028816ea0e45324d068d83d43144adb41da579
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-07-04 09:02:30 +0200

    Update grafos.cpp

--- grafos/grafos.cpp
+++ grafos/grafos.cpp
@@ -10,7 +10,7 @@ float operator-(Node_E const &n1, Node_E const &n2)
     return pow(n1.data.x - n2.data.x, 2) + pow(n1.data.y - n2.data.y, 2); // Sin raiz cuadrada para reducir tiempo de computacion
 }
 
-void push(shared_ptr<Node_E> &n1, shared_ptr<Node_E> &n2, bool bidireccional = false)
+void push(shared_ptr<Node_E> &n1, shared_ptr<Node_E> &n2, bool bidireccional)
 {
     auto edge = Edge{n2, *n2 - *n1}; // Camino entre n1 y n2, con su distancia
     n1->neighbors.insert(edge);
