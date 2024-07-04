fec17a7b7986855c62ed210a969eec645cc38bf3
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-07-04 09:00:29 +0200

    Update grafos.h

--- grafos/grafos.h
+++ grafos/grafos.h
@@ -153,11 +153,7 @@ void dijstra(shared_ptr<Node<T>> &start, vector<shared_ptr<Node<T>>> &nodes)
 
 
 // ESTRELLA
-float operator-(Node_E const &n1, Node_E const &n2)
-{
-    return pow(n1.data.x - n2.data.x, 2) + pow(n1.data.y - n2.data.y, 2); // Sin raiz cuadrada para reducir tiempo de computacion
-}
-
+float operator-(Node_E const &n1, Node_E const &n2);
 
 void push(shared_ptr<Node_E> &n1, shared_ptr<Node_E> &n2, bool bidireccional = false)
 {
