e02c0f94ce7d2d58f253540576d7dce8139f3e99
Daniel Carneros Mateu <109517814+theDn2000@users.noreply.github.com>
2024-07-08 08:33:29 +0200

    classes

---
+++ clases/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+using namespace std;
+
+class Square
+{
+    private:
+        int side;
+    public:
+        Square(int side)
+        {
+            this->side = side;
+        }
+        float getArea()
+        {
+            return side * side;
+        }
+        float getPerimeter()
+        {
+            return 4 * side;
+        }
+        float print()
+        {
+            cout << "Soy un cuadrado de lado " << side << endl;
+        }
+};
+
+class Circle
+{
+    private:
+        int radius;
+    public:
+        Circle(int radius)
+        {
+            this->radius = radius;
+        }
+        float getArea()
+        {
+            return 3.1416 * radius * radius;
+        }
+        float getPerimeter()
+        {
+            return 2 * 3.1416 * radius;
+        }
+        float print()
+        {
+            cout << "Soy un circulo de radio " << radius << endl;
+        }
+};
+
+class Triangle // Equilatero
+{
+    private:
+        int side;
+    public:
+        Triangle(int side)
+        {
+            this->side = side;
+        }
+        float getArea()
+        {
+            return (side * side) / 2;
+        }
+        float getPerimeter()
+        {
+            return 3 * side;
+        }
+        float print()
+        {
+            cout << "Soy un triangulo de lado " << side << endl;
+        }
+};
+
+int main()
+{
+    Square square = Square(5);
+    Circle circle = Circle(5);
+    Triangle triangle = Triangle(5);
+
+    square.print();
+    cout << "Area: " << square.getArea() << endl;
+    cout << "Perimetro: " << square.getPerimeter() << endl;
+
+    circle.print();
+    cout << "Area: " << circle.getArea() << endl;
+    cout << "Perimetro: " << circle.getPerimeter() << endl;
+
+    triangle.print();
+    cout << "Area: " << triangle.getArea() << endl;
+    cout << "Perimetro: " << triangle.getPerimeter() << endl;
+
+    return 0;
+}
